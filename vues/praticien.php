f40e284a0d75306896eabb123e0c72e0a3b1ea22
valerieferrand <95566312+valerieferrand@users.noreply.github.com>
2022-04-05 14:02:48 +0200

    p

--- vues/praticien.php
+++ vues/praticien.php
@@ -5,46 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/styles.css">
-
-    <head>
-    <title></title>
-    <meta charset='utf-8'>
+    <link rel="stylesheet" href="../css/styles.css">   
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  </head>
+  </head>  
 
-    <title>ProMed</title>
-</head>
-<body>
+<body>   
+    
     <div class="wrapper">
-        <nav>
-            <div>
-                <img src="../images/promed_logo.jpg" alt="logo promed">
-                           
-                
-            </div>
+        <nav>                   
+            <img src="../images/promed_logo.jpg" alt="" class="img-rounded pull-right" width="200" >     
+                          
             <div class="button-wrapper">
                 <ul class="nav justify-content-end">
                     <li class="nav-item">
                     
-                        <button type="button" class="btn btn-light border border-primary mr-5">
+                        <button type="button" class="btn btn-light border border-primary mr-4">
                             <a class="nav-link active" href="#">+Fiche patient</a>
                              
-                        </button>
-                        
+                        </button>                       
                        
                     </li>
                     <li class="nav-item">
-                        <button type="button" class="btn btn-light border border-primary mr-5">
+                        <button type="button" class="btn btn-light border border-primary mr-4">
                             <a class="nav-link active" href="#">consulter</a>
                             
                         </button>
                     </li>
                     <li class="nav-item">
-                        <button type="button" class="btn btn-light border border-primary mr-5">
+                        <button type="button" class="btn btn-light border border-primary mr-4">
                             <a class="nav-link active" href="#">parametre</a>
                         </button>
-                        <img src="../images/logout.png" alt="img-rounded pull-right" width="50" >
+                       
+                        <li class="nav-item">  
+
+                        <p><img src="../images/logout.png" alt="" class="img-rounded pull-right" width="85" ></p>   
+                        
+                     
+                        </button>
+
                     </li>
                 </ul>
                 
@@ -52,12 +50,15 @@
           
         </nav>
         <div class="trait"></div>
-        <div class="form-wrapper">
+        <div class="form-wrapper">    
             
         </div>
+        <div class="container">
         <div class="d-flex justify-content-between">
             <span><h3>Bienvenue:</h3></span>
-            
+            <div>
+  <span> </span>
+</div>
             <span><h5><?php setlocale(LC_TIME, 'fr_FR.utf8','fra');
                                         date_default_timezone_set('Europe/Paris');
                                         echo (strftime('%A %d %B %Y '));?></h5>
@@ -68,8 +69,6 @@
        <div class="d-flex justify-content-between"></div>
             <span><h5>Mon activité:</h5></span>                    
   
-</div>
-
      
       <table class="table">
         <thead>
