11ace480d83c16e7a0b129e44fb240aae2848f4a
Maxime <maximearchenoul@gmail.com>
2022-05-01 20:33:29 +0200

    Réorganisation des vues et début d'implémentation des chemins par les controllers

--- vues/accueil_praticien.php
+++ vues/accueil_praticien.php
@@ -1,46 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&amp;display=swap" rel="stylesheet" type="text/css">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/assets/owl.carousel.min.css'>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/assets/owl.theme.default.min.css'>
-    <link rel="stylesheet" href="../static/css/index.css" />
-    <link rel="stylesheet" href="../static/css/responsive.css">
-    </head>
-    <body>
+<?php
 
-        
 
-        <div class="container header">
-            <nav class="navbar navbar-expand-lg border-0">
-                <button class="navbar-toggler header-btn-collapse-nav tetx-white" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toogle Navigation">
-                    <i class="fa fa-bars" aria-hidden="true"></i>
-                </button>
-                <a href="index.html" class="logo"> <img src="../static/images/logo.png" alt="logo"> </a>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
-                    <ul class="header-bottom-menu-list navbar-nav">
-                        <li><a href="">Ajouter fiche patient</a></li>
-                        <li><a href="">Consulter</a></li>
-                        <li><a href="">Paramètres</a></li>
-                        <li><i class="fa-solid fa-right-from-bracket"></i></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-
+?>
         <div class="main-wrapper">
             <section class="connexion">
                 <div class="container">
                     <div class="row mt-3">
                         <div class="col-md-6 col-lg-6 col-xs-12-col-sm-10 pt-5 pb-5">
-                            <h2>Bienvenue Jean Trucmuche</h2>
-                            <h4>Mon activité : <span> Infirmier</span></h4>
+                            <h2><?php echo "Bienvenue"." ".$_SESSION["nomPrenom"]; ?></h2>                         
+                            <h2><?=$test2; ?></h2>     
+                            <h4>Mon activité : <span> <?=$activite?></span></h4>
                         </div>
                     </div>
                 </div>
@@ -106,9 +75,3 @@
             </section>
         </div>
 
-
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/owl.carousel.min.js'></script>
-    </body>
-</html>
